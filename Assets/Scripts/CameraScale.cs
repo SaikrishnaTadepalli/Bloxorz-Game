87c8eac7ea8c5110ae9579b7851f51485853085f
SaikrishnaTadepalli <58894708+SaikrishnaTadepalli@users.noreply.github.com>
2020-03-10 18:31:28 -0400

    Update CameraScale.cs

--- Assets/Scripts/CameraScale.cs
+++ Assets/Scripts/CameraScale.cs
@@ -6,21 +6,14 @@ public class CameraScale : MonoBehaviour
 {
     void Start()
     {
-        // set the desired aspect ratio (the values in this example are
-        // hard-coded for 16:9, but you could make them into public
-        // variables instead so you can set them at design time)
         float targetaspect = 16.0f / 9f;
 
-        // determine the game window's current aspect ratio
         float windowaspect = (float)Screen.width / (float)Screen.height;
 
-        // current viewport height should be scaled by this amount
         float scaleheight = windowaspect / targetaspect;
 
-        // obtain camera component so we can modify its viewport
         Camera camera = GetComponent<Camera>();
 
-        // if scaled height is less than current height, add letterbox
         if (scaleheight < 1.0f)
         {
             Rect rect = camera.rect;
@@ -32,7 +25,7 @@ public class CameraScale : MonoBehaviour
 
             camera.rect = rect;
         }
-        else // add pillarbox
+        else 
         {
             float scalewidth = 1.0f / scaleheight;
 
